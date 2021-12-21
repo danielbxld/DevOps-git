408208a5338b095e2580f0b90042b4be97691ff7
danielbxl <dpn@dpn.eu>
2021-12-21 21:15:15 +0100

    added dog

--- animal.py
+++ animal.py
@@ -2,9 +2,15 @@ import sys
 
 def default():
 	print('Hello')
+def dog():
+	print('Guau')
+
 
 def main(): 
-	default()
+	if sys.argv[1] == 'dog':
+		dog()
+	else:
+		default()
 
 if __name__ == '__main__':
 	main()
